f86bd80ba2e9171a07703365a9688e8036d88b9f
Alexey <symblight@gmail.com>
2023-06-18 12:13:13 +0300

    search

---
+++ src/lib/outlined/SearchOutline.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+export const SearchOutline = props => {
+  return <svg width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="black" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+};
+SearchOutline.displayName = "SearchOutline";
+export default SearchOutline;
