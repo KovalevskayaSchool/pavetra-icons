9f87b6115d318367cb28c5b84c1b69a956c72fd1
Алексей Ткаченко <aleksejtkacenko@MacBook-Pro-Aleksej.local>
2023-06-28 08:01:28 +0300

    icons

---
+++ src/lib/outlined/ChevronLeftDoubleOutline.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+export const ChevronLeftDoubleOutline = props => {
+  return <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M18 17L13 12L18 7M11 17L6 12L11 7" stroke="black" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+};
+ChevronLeftDoubleOutline.displayName = "ChevronLeftDoubleOutline";
+export default ChevronLeftDoubleOutline;
---
+++ src/lib/outlined/ChevronRightDoubleOutline.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+export const ChevronRightDoubleOutline = props => {
+  return <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M6 17L11 12L6 7M13 17L18 12L13 7" stroke="black" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+};
+ChevronRightDoubleOutline.displayName = "ChevronRightDoubleOutline";
+export default ChevronRightDoubleOutline;
