4690a188bd0fd6fa957c15747d0b28551611cef9
Aleksei Tkachenko <symblight@gmail.com>
2023-10-25 19:49:02 +0300

    update

---
+++ src/lib/outlined/CheckOutline.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+export const CheckOutline = props => {
+  return <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor" {...props}><path fill="currentColor" d="M21 7L9 19l-5.5-5.5l1.41-1.41L9 16.17L19.59 5.59L21 7Z" /></svg>;
+};
+CheckOutline.displayName = "CheckOutline";
+export default CheckOutline;
